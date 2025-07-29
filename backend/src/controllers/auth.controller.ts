9d26f2ec16bfa878315bd0e6dea261c218129437
Meriem Zitari <meriem.taguia@gmail.com>
2025-07-30 00:56:27 +0200

    modify cookies configuration to cross site

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -15,8 +15,8 @@ export async function handleRegister(
     //  Enregistrer le token dans un cookie ---
     res.cookie('access_token', token, {
       httpOnly: true, // Empêche l'accès via JavaScript côté client
-      secure: process.env.NODE_ENV === 'production', // N'envoyer le cookie que via HTTPS en production
-      sameSite: 'strict', // Protège contre les attaques CSRF
+      secure: process.env.NODE_ENV === 'production', // quand c'est 'true', n'envoye le cookie que via HTTPS en production
+      sameSite: 'none', // 'strict' Protège contre les attaques CSRF, et 'none' permet de fonctionner en production avec des requêtes cross-site, dans ce cas, il faut que secure soit à 'true'
       maxAge: 3600000 // Durée de vie du cookie en ms (1 heure ici, qui correspond à l'expiration du JWT)
     });
 
--- backend/src/controllers/logout.controller.ts
+++ backend/src/controllers/logout.controller.ts
@@ -6,8 +6,8 @@ export async function handleLogout(req: Request, res: Response) {
       // Supprimer le cookie contenant le token
       res.clearCookie('access_token', {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production',  // quand c'est 'true', n'envoie le cookie que via HTTPS en production
+        sameSite: 'none', // 'strict' Protège contre les attaques CSRF, et 'none' permet de fonctionner en production avec des requêtes cross-site
       });
   
       // Répondre avec un message de succès
